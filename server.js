4e87d0ec79f96d2a131a86a5c074ff32c18aa103
Evan Gacheru <evans.gacheru@andela.com>
2018-12-19 11:31:11 +0300

    project set up

--- server.js
+++ server.js
@@ -1,25 +1,22 @@
 'use strict'
 
 const Hapi = require('hapi')
-const Joi = require("joi");//used for validation
 
 const server = new Hapi.Server({
     "host": "localhost",
     "port": 3000 
 });
 
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: (request, h) => {
+
+        return 'Welcome to the Server Side of the Simple Application!';
+    }
+});
 
 const init = async () => {
-    await server.register(require('inert'));
-
-    server.route({
-        method: 'GET',
-        path: '/home',
-        handler: (request, h) => {
-
-            return h.file('./src/index.html');
-        }
-    });
 
     await server.start();
     console.log(`Server running at: ${server.info.uri}`);
