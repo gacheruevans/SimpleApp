0da369777dc21474f0bf064bf3e6b6fb8f90fc1c
Evan Gacheru <evans.gacheru@andela.com>
2019-01-13 19:05:37 +0300

    code clean up

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,4 @@
 'use strict'
-// Lib imports
-const cookieParser = require('cookie-parser');
-const session = require('express-session');
-
-// Config import
-const config =require('../config/config');
 
 // Controller imports
 const authController = require('../controllers').auth;
@@ -13,43 +7,7 @@ const userController = require('../controllers').user;
 
 module.exports = (app) => {
 
-    // Initialize cookie-parser to allow us access the cookies stored in the browser. 
-    app.use(cookieParser());
-
-
-    // Initialize express-session to allow us track the logged-in user across sessions.
-    app.use(session({
-    key: 'user_sid',
-    secret: config.keySecrete,
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-        expires: 600000
-    }
-    }));
-
-    // Check if user's cookie is still saved in browser and user is not set, then automatically log the user out.
-    app.use((req, res, next) => {
-    if (req.cookies.user_sid && !req.session.user) {
-        res.clearCookie('user_sid');        
-    }
-    next();
-    });
-
-    // middleware function to check for logged-in users
-    let sessionChecker = (req, res, next) => {
-        if (req.session.user && req.cookies.user_sid) {
-            res.redirect('/dashboard');
-        } else {
-            next();
-        }    
-    };
-
     app.get('/api/notes', (req,res) => {
-        // Middleware function to check for logged-in users
-        if (req.session.user && req.cookies.user_sid) {
-            res.redirect('/api/notes');
-        }
         res.status(200).send({
             message: 'welcome to Notes Application please login'
         }) 
@@ -57,7 +15,7 @@ module.exports = (app) => {
 
     app.post('/api/notes/login', authController.login); //login of users
      
-    app.post('/api/notes/register', sessionChecker, authController.register); //register users
+    app.post('/api/notes/register', authController.register); //register users
     app.post('/api/notes/users/:userId/logout', authController.logout);//logout of users
 
     app.get('/api/notes/users', userController.list); //list all users
